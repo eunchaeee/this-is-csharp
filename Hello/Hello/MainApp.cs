978f025b3764da4812c73c7cb07c6faa8296a8b7
Ellie <ecjeong@maxst.com>
2022-12-17 14:31:21 +0900

    01-Hello

---
+++ Hello/Hello/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        // program starts here.
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0}", args[0]);
+        }
+    }
+}
