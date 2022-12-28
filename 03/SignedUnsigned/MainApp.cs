666bf391be5ca43cfbfb03d04c57648cf00a3722
eunchaeee <ddolchae513@gmail.com>
2022-12-28 23:06:33 +0900

    03장-SignedUnsigned

---
+++ 03/SignedUnsigned/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SignedUnsigned
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            byte a = 0b11111111;
+            sbyte b = (sbyte)a;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            byte c = 0b10000001;
+            sbyte d = (sbyte)c;
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
