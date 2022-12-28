c86f81250c7fec50bdb035c5d3f96e4db4b51d44
eunchaeee <ddolchae513@gmail.com>
2022-12-28 23:19:58 +0900

    03장 Overflow

---
+++ 03/Overflow/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Overflow
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            Console.WriteLine(a);
+
+            a = a + 1;      // overflow
+
+            Console.WriteLine(a);
+
+            a = a + 1;      // overflow again
+
+            Console.WriteLine(a);
+        }
+    }
+}
