6c11380713d35554ec2e09d5e2065ca2462ea26f
eunchaeee <ddolchae513@gmail.com>
2023-01-03 23:19:22 +0900

    03장 Char

---
+++ 03/Char/Char.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Char
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.Write(e);
+            Console.WriteLine();
+        }
+    }
+}
